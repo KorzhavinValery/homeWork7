7049f446a2808377df04ea590b6b7e50694107ea
Vel <korzhavin.vel@yandex.ru>
2023-07-05 16:29:25 +0700

    Задание 1

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,21 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+        task1();
+
+
+    }
+
+    public static void task1() {
+        System.out.println("Задача 1");
+        int salary = 15000;
+        int total = 0;
+        int month = 0;
+        while (total <= 2_459_000) {
+            total = total + salary;
+            month = month + 1;
+            System.out.println("Месяц " + month + ", Cумма накоплений равна " + total + " рублей.");
+        }
 
     }
 }
